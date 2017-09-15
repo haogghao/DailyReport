25abe2bb75a4dc0af3b870163186d45b6bf9a779
haogghao <1569677171@qq.com>
2017-09-15 10:30:31 +0800

    update ExcelImage

--- src/com/ExcelImage.java
+++ src/com/ExcelImage.java
@@ -9,53 +9,56 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-import org.apache.poi.hssf.usermodel.HSSFClientAnchor;
-import org.apache.poi.hssf.usermodel.HSSFPatriarch;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFClientAnchor;
 import org.apache.poi.xssf.usermodel.XSSFDrawing;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-public class ExcelImageTest {  
+public class ExcelImage {  
 	public void addNetwork(String targetFile,String sheetName) throws IOException{
 		FileOutputStream fileOut = null;     
         BufferedImage bufferImg = null;     
        //先把读进来的图片放到一个ByteArrayOutputStream中，以便产生ByteArray    
-  //     try {  
+       try {  
            ByteArrayOutputStream byteArrayOut = new ByteArrayOutputStream();     
            bufferImg = ImageIO.read(new File("D:/DailyReportResouceFiles/20170912/CT2.png"));     
-           ImageIO.write(bufferImg, "png", byteArrayOut);  
+           ImageIO.write(bufferImg, "png", byteArrayOut);  //指定图片格式
              
        	   FileInputStream fis = new FileInputStream(targetFile); 
            XSSFWorkbook wb = new XSSFWorkbook(fis);  
            XSSFSheet sheet1 = wb.getSheet(sheetName);   
            //画图的顶级管理器，一个sheet只能获取一个（一定要注意这点）  
            XSSFDrawing patriarch = sheet1.createDrawingPatriarch();     
-           //anchor主要用于设置图片的属性  
-           HSSFClientAnchor anchor = new HSSFClientAnchor(0, 0, 255, 255,(short) 1, 1, (short) 5, 8);     
-           anchor.setAnchorType(3);     
+           //anchor主要用于设置图片的属性  0 0,255 255应该表示图片插入的范围;0 1,25 18 表示图片开始和结束位置
+           XSSFClientAnchor anchor = new XSSFClientAnchor(0, 0, 255, 255,(short) 0, 1, (short) 25, 18);   //5,8  
+           anchor.setAnchorType(3);
+           XSSFClientAnchor anchor1 = new XSSFClientAnchor(0, 0, 255, 255,(short) 0, 20, (short) 25, 37);   //5,8  
+           anchor1.setAnchorType(3); 
+           XSSFClientAnchor anchor2 = new XSSFClientAnchor(0, 0, 255, 255,(short) 0, 40, (short) 25, 57);   //5,8  
+           anchor2.setAnchorType(3); 
            //插入图片    
-           patriarch.createPicture(anchor, wb.addPicture(byteArrayOut.toByteArray(), HSSFWorkbook.PICTURE_TYPE_JPEG));   
+           patriarch.createPicture(anchor, wb.addPicture(byteArrayOut.toByteArray(), XSSFWorkbook.PICTURE_TYPE_PNG));
+           patriarch.createPicture(anchor1, wb.addPicture(byteArrayOut.toByteArray(), XSSFWorkbook.PICTURE_TYPE_PNG)); 
+           patriarch.createPicture(anchor2, wb.addPicture(byteArrayOut.toByteArray(), XSSFWorkbook.PICTURE_TYPE_PNG));
            fileOut = new FileOutputStream("D:/2.xlsx");     
            // 写入excel文件     
             wb.write(fileOut);     
-            System.out.println("----Excle文件已生成------");  
-//       } catch (Exception e) {  
-//           e.printStackTrace();  
-//       }finally{  
-//           if(fileOut != null){  
-//                try {  
-//                   fileOut.close();  
-//               } catch (IOException e) {  
-//                   e.printStackTrace();  
-//               }  
-//           }  
-//       }  
+            System.out.println("----BC,BL,CT图片插入成功------");  
+       } catch (Exception e) {  
+           e.printStackTrace();  
+       }finally{  
+           if(fileOut != null){  
+                try {  
+                   fileOut.close();  
+               } catch (IOException e) {  
+                   e.printStackTrace();  
+               }  
+           }  
+       }  
 		
 	}
     public static void main(String[] args) {  
-    	ExcelImageTest EI=new ExcelImageTest();
+    	ExcelImage EI=new ExcelImage();
     	try {
 			EI.addNetwork("d:/22.xlsx", "Network");
 		} catch (IOException e) {
